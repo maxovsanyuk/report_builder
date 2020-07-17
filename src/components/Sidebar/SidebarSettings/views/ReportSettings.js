8bcd33e3736d72bc5a24595c57de1af86db98bc3
MaksOvs <maksovsanyuk@gmail.com>
2020-07-17 09:42:53 +0300

    add Settings color picker

--- src/components/Sidebar/SidebarSettings/views/ReportSettings.js
+++ src/components/Sidebar/SidebarSettings/views/ReportSettings.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setSettings } from "../../../../redux/actions/app_action";
 
 import styled from "styled-components";
+import { SketchPicker } from "react-color";
 
 // MATERIAL
 
@@ -11,7 +12,6 @@ import InputLabel from "@material-ui/core/InputLabel";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
 import Paper from "@material-ui/core/Paper";
-import ColorPicker from "material-ui-color-picker";
 
 // LODASH
 
@@ -26,28 +26,59 @@ const SettingsBox = styled.div`
   padding: 10px;
 
   .color-picker {
-    width: 60px;
-    margin: 0 10px 0 0;
-
     &:hover {
       cursor: pointer;
     }
   }
 
-  .color-box {
-    min-width: 50px;
-    height: 50px;
+  .close-btn {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 20px;
+    height: 20px;
+    position: absolute;
+    top: -10px;
+    right: -10px;
     border-radius: 50%;
     border: 1px solid #ccc;
+    background: #fff;
+    font-size: 12px;
+    color: #999;
+    z-index: 30;
+    transition: 0.3s;
+    &:hover {
+      transition: 0.3s;
+      cursor: pointer;
+      color: #000;
+    }
+  }
+
+  .color-box {
+    min-width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    border: 1px solid #ccc;
+    transition: 0.6s;
+
+    &:hover {
+      cursor: pointer;
+    }
   }
 `;
 
 const ReportSettings = () => {
   const [currentState, setCurrentState] = useState({});
+  const [colorPickerOpen, setColorPickerOpen] = useState(false);
+  const [backgroundColorPickerOpen, setBackgroundColorPickerOpen] = useState(
+    false
+  );
 
   const state = useSelector((state) => state.app);
   const { settings } = state;
   const dispatch = useDispatch();
+
+  console.log(settings, "SSSS");
 
   return (
     <SettingsBox>
@@ -107,95 +138,135 @@ const ReportSettings = () => {
             style={{
               display: "flex",
               alignItems: "center",
-              width: "fit-content",
-              margin: "0 20px 0 0",
+              margin: "20px 20px 0 0",
+              position: "relative",
             }}
           >
-            <span style={{ margin: "0 10px 0 0" }}>Pick Color</span>
-            <FormControl>
-              <ColorPicker
-                className="color-picker"
-                name="color"
-                defaultValue={
-                  get(currentState, "basic.color") ||
-                  get(settings, "reportSettings.basic.color") ||
-                  "#CCC"
-                }
-                onChange={(color) => {
-                  setCurrentState({
-                    ...currentState,
-                    basic: { ...currentState?.basic, color },
-                  });
+            <span style={{ margin: "0 10px 0 0" }}>Color</span>
 
-                  dispatch(
-                    setSettings({
-                      ...settings,
-                      reportSettings: {
-                        ...settings?.reportSettings,
-                        basic: {
-                          ...settings?.reportSettings?.basic,
-                          color,
-                        },
-                      },
-                    })
-                  );
+            {colorPickerOpen && (
+              <div
+                style={{
+                  position: "absolute",
+                  top: "-30px",
+                  left: "120px",
+                  zIndex: 20,
                 }}
-              />
-            </FormControl>
+              >
+                <span
+                  className="close-btn"
+                  onClick={() => setColorPickerOpen(!colorPickerOpen)}
+                >
+                  X
+                </span>
+                <SketchPicker
+                  className="color-picker"
+                  color={
+                    get(currentState, "basic.color") ||
+                    get(settings, "reportSettings.basic.color") ||
+                    "#CCC"
+                  }
+                  onChangeComplete={(color) => {
+                    setCurrentState({
+                      ...currentState,
+                      basic: { ...currentState?.basic, color },
+                    });
+
+                    dispatch(
+                      setSettings({
+                        ...settings,
+                        reportSettings: {
+                          ...settings?.reportSettings,
+                          basic: {
+                            ...settings?.reportSettings?.basic,
+                            color,
+                          },
+                        },
+                      })
+                    );
+                  }}
+                />
+              </div>
+            )}
+
             <div
+              onClick={() => setColorPickerOpen(!colorPickerOpen)}
               className="color-box"
               style={{
                 background:
-                  get(currentState, "basic.color") ||
-                  get(settings, "reportSettings.basic.color") ||
+                  get(currentState, "basic.color.hex") ||
+                  get(settings, "reportSettings.basic.color.hex") ||
                   "#CCC",
               }}
             />
           </div>
+
           <div
             style={{
               display: "flex",
               alignItems: "center",
               width: "fit-content",
+              margin: "20px 0 0 0",
+              position: "relative",
             }}
           >
             <span style={{ margin: "0 10px 0 0" }}>Pick Background Color</span>
+            {backgroundColorPickerOpen && (
+              <div
+                style={{
+                  position: "absolute",
+                  top: "-30px",
+                  left: "120px",
+                  zIndex: 20,
+                }}
+              >
+                <span
+                  className="close-btn"
+                  onClick={() =>
+                    setBackgroundColorPickerOpen(!backgroundColorPickerOpen)
+                  }
+                >
+                  X
+                </span>
+                <SketchPicker
+                  className="color-picker"
+                  color={
+                    get(currentState, "basic.backgroundColor.hex") ||
+                    get(settings, "reportSettings.basic.backgroundColor.hex") ||
+                    "#CCC"
+                  }
+                  onChangeComplete={(color) => {
+                    setCurrentState({
+                      ...currentState,
+                      basic: { ...currentState?.basic, backgroundColor: color },
+                    });
 
-            <ColorPicker
-              className="color-picker"
-              name="color"
-              value={get(currentState, "basic.backgroundColor")}
-              defaultValue={
-                get(currentState, "basic.backgroundColor") ||
-                get(settings, "reportSettings.basic.backgroundColor") ||
-                "#CCC"
-              }
-              onChange={(color) => {
-                setCurrentState({
-                  ...currentState,
-                  basic: { ...currentState?.basic, backgroundColor: color },
-                });
+                    dispatch(
+                      setSettings({
+                        ...settings,
+                        reportSettings: {
+                          ...settings?.reportSettings,
+                          basic: {
+                            ...settings?.reportSettings?.basic,
+                            backgroundColor: color,
+                          },
+                        },
+                      })
+                    );
+                  }}
+                />
+              </div>
+            )}
 
-                dispatch(
-                  setSettings({
-                    ...settings,
-                    reportSettings: {
-                      ...settings?.reportSettings,
-                      basic: {
-                        ...settings?.reportSettings?.basic,
-                        backgroundColor: color,
-                      },
-                    },
-                  })
-                );
-              }}
-            />
             <div
+              onClick={() =>
+                setBackgroundColorPickerOpen(!backgroundColorPickerOpen)
+              }
               className="color-box"
               style={{
                 background:
-                  get(currentState, "basic.backgroundColor") ||
-                  get(settings, "reportSettings.basic.backgroundColor") ||
+                  get(currentState, "basic.backgroundColor.hex") ||
+                  get(settings, "reportSettings.basic.backgroundColor.hex") ||
                   "#CCC",
               }}
             />
@@ -312,7 +383,6 @@ const ReportSettings = () => {
             })}
           </Select>
         </FormControl>
-
         <TextField
           style={{
             width: "180px",
@@ -347,7 +417,6 @@ const ReportSettings = () => {
             );
           }}
         />
-
         <FormControl>
           <InputLabel htmlFor="source-label">Repeat</InputLabel>
 
