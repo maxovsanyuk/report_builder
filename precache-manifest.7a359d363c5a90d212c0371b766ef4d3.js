994fe15cb67f2b699faff04e52e9bf6ea4a0c56e
MaksOvs <maksovsanyuk@gmail.com>
2020-09-02 21:45:00 +0300

    Updates

---
+++ precache-manifest.7a359d363c5a90d212c0371b766ef4d3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bac699dbf68ff6a5adb9c5a11e18e748",
+    "url": "/report_builder/index.html"
+  },
+  {
+    "revision": "827e3ab0e4f34c7a715a",
+    "url": "/report_builder/static/css/2.2ce93ba3.chunk.css"
+  },
+  {
+    "revision": "827e3ab0e4f34c7a715a",
+    "url": "/report_builder/static/js/2.846a11d7.chunk.js"
+  },
+  {
+    "revision": "729937898630e839994162d5d9f8ce89",
+    "url": "/report_builder/static/js/2.846a11d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9174f9b758c159518f01",
+    "url": "/report_builder/static/js/main.d037d2c5.chunk.js"
+  },
+  {
+    "revision": "a228cb7b9f1b772c5ab8",
+    "url": "/report_builder/static/js/runtime-main.f3a02530.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/report_builder/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "0a2b08ae41e1c8924b7c4fe507d29142",
+    "url": "/report_builder/static/media/grid.0a2b08ae.png"
+  },
+  {
+    "revision": "f45e90d285a25dd2f578a113d8d54021",
+    "url": "/report_builder/static/media/menu-icon.f45e90d2.png"
+  },
+  {
+    "revision": "dcf8c7fcb1965c89af7db608bce03a2c",
+    "url": "/report_builder/static/media/resizible.dcf8c7fc.png"
+  }
+]);
