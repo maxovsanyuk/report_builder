5547655f0aa0dd2ce46151c8dc1255271b487382
MaksOvs <maksovsanyuk@gmail.com>
2020-09-03 19:49:59 +0300

    Init com

--- src/components/Sidebar/SidebarSettings/views/widgetComponent/DefineWidgetSettingsComp.js
+++ src/components/Sidebar/SidebarSettings/views/widgetComponent/DefineWidgetSettingsComp.js
@@ -11,29 +11,13 @@ const DefineWidgetSettingsComp = ({ choosenWidget }) => {
       return <ImgSettingsComp choosenWidget={choosenWidget} />;
     case "line":
       return <LineSettingsComp choosenWidget={choosenWidget} />;
-    case "rectangle":
-      return <ImgSettingsComp />;
-    case "text_box":
-      return <ImgSettingsComp />;
+    // case "rectangle":
+    //   return <ImgSettingsComp />;
+    // case "text_box":
+    //   return <ImgSettingsComp />;
     default:
       return `${name} comming soon`;
   }
 };
 
 export default DefineWidgetSettingsComp;
-
-// const DEFAULT_WG_POSITION_TOP = 10;
-// const DEFAULT_WG_POSITION_LEFT = 10;
-//
-// basicSettings: { name: "Source", value: "" },
-// linkTo: null,
-//     appearence: { border: "solid" },
-// size: {
-//     sizing: { width: DEFAULT_WG_WEIGHT, height: DEFAULT_WG_HEIGHT },
-// },
-// position: {
-//     top: DEFAULT_WG_POSITION_TOP,
-//         left: DEFAULT_WG_POSITION_LEFT,
-// },
-// visibility: { visible: true },
-// miscellaneous: { tooltip: [] },
